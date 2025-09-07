fa77f7631e91f067b41f59c9f04d7bba9ce0c401
Abdallah-Alqiran <abdallahalqiran765@gmial.com>
2025-09-07 15:38:30 +0300

    improve meal details screen

--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/core/utils/app_color.dart';
@@ -6,6 +6,7 @@ import 'package:meals_app/core/utils/app_color.dart';
 class AppTextStyles {
   static TextStyle onBoardingTitleStyle = GoogleFonts.inter(fontSize: 32.sp, fontWeight: FontWeight.w700, color: AppColor.title);
   static TextStyle onBoardingDescriptionStyle = GoogleFonts.inter(fontSize: 14.sp, fontWeight: FontWeight.normal, color: AppColor.white);
+  static TextStyle foodTitle = GoogleFonts.inter(fontSize: 24.sp, fontWeight: FontWeight.w400, color: Colors.black);
 
   static TextStyle white14SemiBold = GoogleFonts.inter(fontSize: 14.sp, fontWeight: FontWeight.w500, color: AppColor.white);
   static TextStyle black16Medium = GoogleFonts.inter(fontSize: 16.sp, fontWeight: FontWeight.w500, color: AppColor.decorationColor);
